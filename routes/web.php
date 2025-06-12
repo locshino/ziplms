c99b8d8cfc05c007ddfeeaa1d09ca2dd35f05057
Nguyễn Tiến Lộc <locshino123@gmail.com>
2025-06-12 23:02:26 +0700

    Add opcache gui

--- routes/web.php
+++ routes/web.php
@@ -11,12 +11,21 @@ Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
+Volt::route('/initial-setup', 'initial-setup')
+    ->name('initial-setup');
+
 Route::middleware(['auth'])->group(function () {
     Route::redirect('settings', 'settings/profile');
 
-    Volt::route('settings/profile', 'settings.profile')->name('settings.profile');
-    Volt::route('settings/password', 'settings.password')->name('settings.password');
-    Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
+    Volt::route('settings/profile', 'settings.profile')
+        ->name('settings.profile');
+
+    Volt::route('settings/password', 'settings.password')
+        ->name('settings.password');
+
+    Volt::route('settings/appearance', 'settings.appearance')
+        ->name('settings.appearance');
 });
 
 require __DIR__.'/auth.php';
+require __DIR__.'/web/opcache-gui.php';
