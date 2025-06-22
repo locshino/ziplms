c653f74ca92553e25b6170817b14c0560e276125
Nguyễn Tiến Lộc <locshino123@gmail.com>
2025-06-22 21:29:21 +0700

    exports-download route

--- routes/web/download.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use App\Http\Controllers\DownloadController;
-
-Route::get('/download/export/{path}.{extension}', [DownloadController::class, 'downloadExport'])
-    ->name('exports.download')
-    ->middleware(['web', 'auth']); // Đảm bảo người dùng đã đăng nhập
