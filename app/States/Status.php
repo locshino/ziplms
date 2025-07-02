2fd12546e567c60357c05a2474ba10ff0ea024ef
duong1259 <131975417+duong1259@users.noreply.github.com>
2025-07-02 23:15:01 +0700

    status

--- app/States/Status.php
+++ app/States/Status.php
@@ -2,23 +2,30 @@
 
 namespace App\States;
 
-use App\States\Active;
-use App\States\Inactive;
 use Spatie\ModelStates\State;
 use Spatie\ModelStates\StateConfig;
 
 abstract class Status extends State
 {
     // Define methods here that are available for all states.
-    abstract public function label(): string;
+    abstract public static function label(): string;
 
-    abstract public function color(): string; // Example of a shared abstract method
+    abstract public static function color(): string; // Example of a shared abstract method
 
+    // Use the config() method to define transitions and the default state
     public static function config(): StateConfig
     {
         return parent::config()
             ->default(Active::class)
             ->allowTransition(Inactive::class, Active::class)
             ->allowTransition(Active::class, Inactive::class);
+    }
+
+    public static function getStates(): array
+    {
+        return [
+            Active::class,
+            Inactive::class,
+        ];
     }
 }
