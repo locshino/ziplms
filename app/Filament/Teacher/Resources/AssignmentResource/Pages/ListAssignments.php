570e8d2ebeb1ecefa2573a1c94f97803db8276e5
TuanTran2005 <tuantvpn00107@gmail.com>
2025-07-23 01:59:21 +0700

    tranvantuan

---
+++ app/Filament/Teacher/Resources/AssignmentResource/Pages/ListAssignments.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Filament\Teacher\Resources\AssignmentResource\Pages;
+
+use App\Filament\Teacher\Resources\AssignmentResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ListRecords;
+
+class ListAssignments extends ListRecords
+{
+    use ListRecords\Concerns\Translatable;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\LocaleSwitcher::make(),
+            Actions\CreateAction::make(),
+        ];
+    }
+
+    protected static string $resource = AssignmentResource::class;
+}
